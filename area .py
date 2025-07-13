5bd2de83ecf12ae53cb4378bfb15d3fcf3aa23d1
Souravnayak0001 <souravnayak0001@gmail.com>
2025-07-13 20:30:07 +0530

    Add files via upload

---
+++ area .py
@@ -0,0 +1,34 @@
+# def areasqare(side):
+#     area =side*side
+#     return  area
+# def main():
+#    arearect=int(input('enter side'))
+#    arearectangle=areasqare(arearect)
+#    print(arearectangle)
+#
+#
+# if __name__ == '__main__':
+#     main()
+#
+#
+# def number_pyramid(n):
+#     for i in range(1, n + 1):
+#         print(' ' * (n - i) + ' '.join(str(x) for x in range(1, i + 1)))
+#
+# number_pyramid(5)
+
+# Input: number of terms
+# n = int(input("Enter the number of terms: "))
+#
+# # Initialize the first two terms
+# a, b = 0, 1
+#
+# # Print the Fibonacci sequence
+# if n >= 1:
+#     print("Fibonacci sequence:")
+#     for _ in range(n):
+#         print(a)
+#         a, b = b, a + b
+# else:
+#     print("Please enter a number greater than or equal to 1.")
+#
